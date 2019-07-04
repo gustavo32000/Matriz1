143612ec686050772059e5e3c2feaa418207c998
gugu <gustavocorrea2032@gmail.com>
2019-07-04 09:24:50 -0300

    Começo do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Matriz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
